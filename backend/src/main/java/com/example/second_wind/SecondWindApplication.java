a9fa5bce88623926e06e295bbb2f65ae2926cc62
Knightwing1941 <pavik14@live.com>
2026-05-16 20:05:12 -0500

    Initial commit

---
+++ backend/src/main/java/com/example/second_wind/SecondWindApplication.java
@@ -0,0 +1,13 @@
+package com.example.second_wind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondWindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondWindApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/second_wind/SecondWindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_wind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondWindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
